bca5d9568d4765e4c1bcfac98ceb799ae5240ac4
KIT-ST <jjg3389@naver.com>
2023-11-30 04:41:30 +0900

    초기 커밋.

---
+++ WPFTETRIS/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace WPFTETRIS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            KeyDown += AnyKeyPressed; //MainWindow에 Keydown 이벤트 핸들러 
+        }
+
+        private void AnyKeyPressed(object sender, KeyEventArgs e)
+        {
+            //MoveToMenuPage
+            MenuPage menuPage = new MenuPage();
+            menuPage.Title = "Menu";
+            this.Content = menuPage;
+        }
+    }
+}
---
+++ WPFTETRIS/MenuPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTETRIS
+{
+    /// <summary>
+    /// MenuPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            InitializeComponent();            
+        }
+
+        private void btn1_click(object sender, RoutedEventArgs e) //1인용
+        {
+            //player.NavigationService.Navigate(new Uri("/singlePlay.xaml", UriKind.Relative));
+            player.Navigate(new SinglePlay());
+        }
+
+        private void btn2_click(object sender, RoutedEventArgs e) //2인용
+        {
+            //player.NavigationService.Navigate(new Uri("/MutiPlay.xaml", UriKind.Relative));
+            player.Navigate(new MutiPlay());
+        }
+
+        private void btn3_click(object sender, RoutedEventArgs e) //AI
+        {
+            //player.NavigationService.Navigate(new Uri("/AI.xaml", UriKind.Relative));
+            player.Navigate(new AI());
+        }
+    }
+}
